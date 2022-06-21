92af75940b3c6ab292b5d6f89566b55b771f1022
lizzethgd <lizzethgd@gmail.com>
2022-06-21 14:46:10 +0300

    heroku push 3

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -13,7 +13,7 @@ export default ({ children }) => {
     (async () => { 
         try{
         await checkAuthentication().then(data => {
-            setUser(data.user ? data.user : 'guest')
+            setUser(data.user ? data.user : {user: 'guest'})
             setIsAuthenticated(data.isAuthenticated? data.isAuthenticated  : false)
             setIsLoaded(true)
         })
