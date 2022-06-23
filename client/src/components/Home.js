6a5aa9eeb243c37d484b6f42b9f68360649f1690
lizzethgd <lizzethgd@gmail.com>
2022-06-23 13:53:29 +0300

    heroku push 01

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import AllRecipes from './AllRecipes';
 
 const Home = () => {
 return (
-<div className="w3-light-green w3-center">   
+<div className="w3-light-green w3-center w3-padding-top-48">   
     <Timeline />
     <AllRecipes />
 </div>
