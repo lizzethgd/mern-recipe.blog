3ea9264ace901a6a2a7628a6e9547aef668a0cd1
lizzethgd <lizzethgd@gmail.com>
2022-07-12 13:29:57 +0300

    heroku push 008

--- client/src/hocs/PrivateRoute.js
+++ client/src/hocs/PrivateRoute.js
@@ -1,20 +1,15 @@
 import {useContext} from 'react';
-import {Route, Navigate} from 'react-router-dom';
+import {Navigate, Outlet, useLocation} from 'react-router-dom';
 import {AuthContext} from '../context/AuthContext';
 
 const PrivateRoute = ({component : Component, ...rest})=>{
     
     const { isAuthenticated, user} = useContext(AuthContext);
     console.log(user)
+    const location = useLocation();
   
     return(
-        <Route {...rest} render={props =>{
-            if(!isAuthenticated)
-                return <Navigate to={{ pathname: '/login', 
-                                       state : {from : props.location}}}/>
-            
-        return <Component {...props}/>
-    }}/>
+        !isAuthenticated ? <Navigate to='/login' state={{ from: location }} replace /> :  <Outlet />  
     )
 }
 
--- client/src/hocs/UnPrivateRoute.js
+++ client/src/hocs/UnPrivateRoute.js
@@ -1,13 +1,15 @@
 import {useContext} from 'react';
-import {Navigate,  Outlet} from 'react-router-dom';
+import {Navigate,  Outlet, useLocation} from 'react-router-dom';
 import {AuthContext} from '../context/AuthContext';
 
 const UnPrivateRoute = ()=>{
     
     const {isAuthenticated} = useContext(AuthContext);
     console.log(isAuthenticated)
+    const location = useLocation();
+
     return(
-        !isAuthenticated ? <Outlet />:  <Navigate to='/' replace/>
+        !isAuthenticated ? <Outlet />:  <Navigate to='/' state={{ from: location }} replace/>
           /*    <Route {...rest} render={props =>{
             if(isAuthenticated) 
             return <Navigate to={{ pathname: '/', 
