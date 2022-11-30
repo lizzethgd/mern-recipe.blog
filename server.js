6a974beade9556ab71965008126a22e52a7b0845
lizzethgd <lizzethgd@gmail.com>
2022-11-30 13:35:54 +0200

    render deploy 8

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 //to understand the form dates
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/client/build')));
-
+app.use(express.urlencoded({extended: false})) 
 app.use(cors());
 app.use(cookieParser())
 
